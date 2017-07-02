d2088edb66cb1d7b1c6adfe74fedb66b5b31ac4b
Robert Edwards <edwards@jlab.org>
2017-07-01 20:16:24 -0400

    Rearranged tests, now do not existing files.

--- tests/test_niledb.nim
+++ tests/test_niledb.nim
@@ -4,6 +4,7 @@ import niledb, tables,
        serializetools/serializebin, serializetools/serialstring
 import unittest
 import strutils, posix, os, hashes
+import random
   
 # Useful for debugging
 proc printBin(x:string): string =
@@ -40,59 +41,17 @@ proc hash(x: KeyPropElementalOperator_t): Hash =
 #
 suite "Tests of DB functions":
   # Hold onto this
-  var meta:string
+  var meta:string = """<?xml version="1.0"?>
 
-  #--------------------------------
-  test "Test reading an existing DB":
-    # Need to declare something
-    echo "Declare conf db"
-    var db = newConfDataStoreDB()
+<DBMetaData>
+  <id>propElemOp</id>
+  <lattSize>4 4 4 16</lattSize>
+  <decay_dir>3</decay_dir>
+</DBMetaData>
+"""
 
-    # Open a file
-    let file = "prop.sdb"
-    echo "Is file= ", file, "  present?"
-    if fileExists(file):
-      echo "hooray, it exists"
-    else:
-      quit("oops, does not exist")
-
-    echo "open the db = ", file
-    var ret = db.open(file, O_RDONLY, 0o400)
-    echo "return type= ", ret
-    if ret != 0:
-      quit("strerror= " & $strerror(errno))
-
-    # Try metadata
-    echo "Get metadata"
-    meta = db.getUserdata()
-    echo "it did not blowup..."
-  
-    # Tests
-    if false:
-      # Read all the keys
-      echo "try getting all the binary keys"
-      let all_keys = db.allBinaryKeys()
-      echo "binary: found num keys= ", all_keys.len
-      echo "here is the first binary key: len= ", all_keys[0].len, "  val= ", printBin(all_keys[0])
-
-      # Deserialize the first key
-      echo "Deserialize first binary key..."
-      let foo = deserializeBinary[KeyPropElementalOperator_t](all_keys[0])
-      echo "here it is:\n", foo
-
-    # Read all the keys
-    if true:
-      echo "try getting all the deserialized keys"
-      let des_keys = allKeys[KeyPropElementalOperator_t](db)
-      echo "found num all keys= ", des_keys.len
-      echo "here is the first all key: len= ", des_keys.len, "  val= ", des_keys[0]
-      #for i in 0..des_keys.len-1:
-      for i in 0..100:
-        echo "k[",i,"]= ", des_keys[i]
-
-    # Close
-    ret = db.close()
-    require(ret == 0)
+  # File name for tests
+  let out_file = "foo.sdb"  
 
 
   #--------------------------------
@@ -102,7 +61,6 @@ suite "Tests of DB functions":
     var db = newConfDataStoreDB()
 
     # Let us write a file
-    let out_file = "foo.sdb"
     echo "Is file= ", out_file, "  present?"
     if fileExists(out_file):
       echo "It exists, so remove it"
@@ -128,15 +86,20 @@ suite "Tests of DB functions":
     # Write stuff
     var kv = initTable[KeyPropElementalOperator_t,float]()
 
-    let val:float = 5.7
-    for t_slice in 9..10:
-      for sl in 0..3:
-        for sr in 0..3:
-          echo "t_slice= ", t_slice, " sl= ", sl, " sr= ", sr
-          let key = KeyPropElementalOperator_t(t_slice: cint(t_slice), t_source: 5, 
-                                               spin_l: cint(sl), spin_r: cint(sr), 
-                                               mass_label: SerialString("fred"))
-          add(kv,key,val)
+    # labels
+    let labels = @["fred", "george"]
+
+    # Build up some keys
+    for ll in items(labels):
+      for t_slice in 9..10:
+        for sl in 0..3:
+          for sr in 0..3:
+            echo "t_slice= ", t_slice, " sl= ", sl, " sr= ", sr
+            let key = KeyPropElementalOperator_t(t_slice: cint(t_slice), t_source: 5, 
+                                                 spin_l: cint(sl), spin_r: cint(sr), 
+                                                 mass_label: SerialString(ll))
+            let val = random(3.0)  # 
+            add(kv,key,val)
 
     # insert the entire table
     ret = db.insert(kv)
@@ -189,4 +152,58 @@ suite "Tests of DB functions":
     # Close
     ret = db.close()
     require(ret == 0)
+
+
+  #--------------------------------
+  test "Test reading an existing DB":
+    # Need to declare something
+    echo "Declare conf db"
+    var db = newConfDataStoreDB()
+
+    # Open a file
+    echo "Is file= ", out_file, "  present?"
+    if fileExists(out_file):
+      echo "hooray, it exists"
+    else:
+      quit("oops, does not exist")
+
+    echo "open the db = ", out_file
+    var ret = db.open(out_file, O_RDONLY, 0o400)
+    echo "return type= ", ret
+    if ret != 0:
+      quit("strerror= " & $strerror(errno))
+
+    # Try metadata
+    echo "Get metadata"
+    meta = db.getUserdata()
+    echo "it did not blowup..."
+  
+    # Tests
+    if true:
+      # Read all the keys
+      echo "try getting all the binary keys"
+      let all_keys = db.allBinaryKeys()
+      echo "binary: found num keys= ", all_keys.len
+      echo "here is the first binary key: len= ", all_keys[0].len, "  val= ", printBin(all_keys[0])
+
+      # Deserialize the first key
+      echo "Deserialize first binary key..."
+      let foo = deserializeBinary[KeyPropElementalOperator_t](all_keys[0])
+      echo "here it is:\n", foo
+
+    # Read all the keys
+    if true:
+      echo "try getting all the deserialized keys"
+      let des_keys = allKeys[KeyPropElementalOperator_t](db)
+      echo "found num all keys= ", des_keys.len
+      echo "here is the first all key: len= ", des_keys.len, "  val= ", des_keys[0]
+      #for i in 0..des_keys.len-1:
+      for i in 0..10:
+        echo "k[",i,"]= ", des_keys[i]
+
+    # Close
+    ret = db.close()
+    require(ret == 0)
+
+
 
